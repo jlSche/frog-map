31a5e0c1dd14ec1162f5f9e7f70922ab65ecffa8
jlsche <jlinsche@gmail.com>
2016-01-30 00:51:42 +0800

    crawler.py ver.1

--- crawler.py
+++ crawler.py
@@ -11,42 +11,49 @@ import pandas as pd
 from lxml import etree
 
 def main():
-  url = 'http://iesn.tfri.gov.tw/forestDW/Frog/Listen?frogSpeciesId=&siteId=02&pointId=&startDate=&endDate=&p=1'
-  start_page_num = 1
-  end_page_num = 152
+  start_page = 1
+  end_page = 152
+
+  df = crawWebsite(start_page, end_page)
+  df.to_csv('./frog.csv', encoding='big5', index=False)
+  
+
+def crawWebsite(start_page=1, end_page=1, start_date='', end_date='', specie='', place=''):
+  main_url = 'http://iesn.tfri.gov.tw/forestDW/Frog/Listen?frogSpeciesId=&siteId=02&pointId=&startDate=&endDate=&p='
   frog = dict()
   frog_list = list()
-  
-  req = ul2.Request(url)
-  response =  ul2.urlopen(req)
-  html = response.read()
 
-  page = etree.HTML(html)
-  
-  species = page.xpath(u"//div[@class='frog-sound-item']//p/text()")
-  info = page.xpath(u"//div[@class='frog-sound-item']//h3/text()")
-  audio_source = page.xpath(u"//div[@class='frog-sound-item']//a[@class='k-button'][2]/@href")
-  
-  for idx in range(0, len(species)):
-    # if more than 2 frog
-    names = re.sub('[\r\n\t ]', '', species[idx])
-    names = names.split(',')
+  for idx in range(start_page, end_page+1):
+    url = main_url + str(idx)
+    req = ul2.Request(url)
+    response =  ul2.urlopen(req)
+    html = response.read()
+    page = etree.HTML(html)
     
-    for jdx in range(0, len(names)):
-      frog = {
-        'specie': re.sub('[\r\n\t ]', '', names[jdx]),
-        'time': datetime.datetime.strptime(info[idx][:19], '%Y-%m-%d %H:%M:%S'),
-        'place': re.sub('[, ]', '', info[idx][21:-3]),
-        'audio_record': audio_source[idx]
-      }
-    
-      frog['time'] = datetime.datetime.strptime(info[idx][:19], '%Y-%m-%d %H:%M:%S')
-      frog['place'] = re.sub('[, ]', '', info[idx][21:-3])
-      frog['audio_record'] = audio_source[idx]
-      frog_list.append(frog)
+    species = page.xpath(u"//div[@class='frog-sound-item']//p/text()")
+    info = page.xpath(u"//div[@class='frog-sound-item']//h3/text()")
+    link = page.xpath(u"//div[@class='frog-sound-item']//a[@class='k-button'][2]/@href")
 
-  df = pd.DataFrame(frog_list)
-  print df
+    for idx in range(0, len(species)):
+      # if more than 2 frog
+      names = re.sub('[\r\n\t ]', '', species[idx])
+      names = names.split(',')
+      
+      for jdx in range(0, len(names)):
+        frog = {
+          'specie': re.sub('[\r\n\t ]', '', names[jdx]),
+          'time': datetime.datetime.strptime(info[idx][:19], '%Y-%m-%d %H:%M:%S'),
+          'place': re.sub('[, ]', '', info[idx][21:-3]),
+          'link': link[idx]
+        }
+        '''
+        frog['time'] = datetime.datetime.strptime(info[idx][:19], '%Y-%m-%d %H:%M:%S')
+        frog['place'] = re.sub('[, ]', '', info[idx][21:-3])
+        frog['link'] = link[idx]
+        '''
+        frog_list.append(frog)
+
+  return pd.DataFrame(frog_list)
 
 
 if __name__ == '__main__':
